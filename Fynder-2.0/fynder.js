e897eba63a912cfe5910bf04d66b286c35b0486f
urriolaandres690 <urriolaandres690@gmail.com>
2026-07-08 00:42:53 -0500

    Fynder-2.0 | 2026-07-08 00:42:53

--- Fynder-2.0/fynder.js
+++ Fynder-2.0/fynder.js
@@ -994,8 +994,7 @@ function _initCarouselDrag(id) {
 
 function buildHome(){
   document.getElementById('featuredGrid').innerHTML=BUSINESSES.filter(b=>b.isFeatured).map(gridCardHTML).join('');
-  document.getElementById('popularList').innerHTML=BUSINESSES.filter(b=>b.isPopular).map(gridCardHTML).join('');
-  _initCarouselDrag('popularList');
+  document.getElementById('popularList').innerHTML=BUSINESSES.filter(b=>b.isPopular).slice(0,4).map(listCardHTML).join('');
   _initCarouselDrag('featuredGrid');
 
   // ── Cifras reales calculadas desde BUSINESSES y REVIEWS ──
