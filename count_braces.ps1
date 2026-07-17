4f2588bc6373323be008884eb46cba4d3c86ec32
urriolaandres690 <urriolaandres690@gmail.com>
2026-07-17 01:52:39 -0500

    Fynder-2.0 | 2026-07-17 01:52:39

--- count_braces.ps1
+++
@@ -1,7 +0,0 @@
-$content = Get-Content -Path ".\Fynder-3.0\pages\mensajes\mensajes.js" -Raw -Encoding UTF8
-$o = 0; $c = 0
-foreach ($ch in $content.ToCharArray()) {
-    if ($ch -eq '{') { $o++ }
-    elseif ($ch -eq '}') { $c++ }
-}
-Write-Host "Abiertas: $o  Cerradas: $c  Diferencia: $($o - $c)"
